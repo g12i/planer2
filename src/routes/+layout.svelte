7337dd8f67f2bddcbd95a247a57fd0923d03e7c1
Wojciech Grzebieniowski <grzebieniowski@gmail.com>
2026-05-19 14:12:35 +0200

    feat: Add tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { QueryClientProvider } from '@tanstack/svelte-query';
 	import { onMount } from 'svelte';
 
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import TooltipProvider from '$lib/components/ui/tooltip-provider.svelte';
+	import { queryClient } from '$lib/query-client';
 	import { initTheme } from '$lib/theme.svelte';
 	import type { LayoutProps } from './$types';
 
@@ -14,6 +16,8 @@
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
-<TooltipProvider>
-	{@render children()}
-</TooltipProvider>
+<QueryClientProvider client={queryClient}>
+	<TooltipProvider>
+		{@render children()}
+	</TooltipProvider>
+</QueryClientProvider>
